591c932abf07da0226618f6d67336a67c2a52aa1
nancyhawa <hawa.nancy@gmail.com>
2015-11-19 10:52:11 -0500

    refactor shipment instance methods

--- lib/app/models/shipment.rb
+++ lib/app/models/shipment.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'pry'
 require_relative 'warehouse'
 
 class Shipment
@@ -10,23 +9,28 @@ class Shipment
   end
 
   def fastest_warehouse
-    result = nil
-    fastest = Float::INFINITY
+    @result = nil
+    @fastest = Float::INFINITY
+    parse_api
+    return_result_or_raise_error
+  end
+
+  def parse_api
     data = JSON.parse(File.read('lib/app/assets/transit_times_api.rb'))
     data.each do |shipment|
-      # binding.pry
-      if shipment['destination'] == @zip && shipment['transit_time'] <= fastest
-        fastest = shipment['transit_time']
-        result = Warehouse.create_by_zip_from_api(shipment['origin'])
+      if shipment['destination'] == @zip && shipment['transit_time'] <= @fastest
+        @fastest = shipment['transit_time']
+        @result = Warehouse.create_by_zip_from_api(shipment['origin'])
       end
     end
+  end
 
-    if result
-      result
+  def return_result_or_raise_error
+    if @result
+      @result
     else
       raise "The zipcode #{@zip} is not in our database."
     end
   end
-end
 
-Shipment.new('55555').fastest_warehouse.display
+end
